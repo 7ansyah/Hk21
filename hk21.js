54ee1db164e0a3dc3e6af10302d69dc7e993341f
Tiren74n <40044281+7ansyah@users.noreply.github.com>
2024-09-06 12:44:34 +0700

    Update hk21.js

--- hk21.js
+++ hk21.js
@@ -4,68 +4,75 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const games = {
         1: {
-            name: 'Riding Extreme 3D',
-            appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
-            promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
-            eventsDelay: 21000,
-            attemptsNumber: 22,
+            name: 'Zoopolis',
+            appToken: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+            promoId: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+            timing: 30000, // 20 seconds
+            attempts: 30,
+        },
+        2: {
+            name: 'Fluff Crusade',
+            appToken: '112887b0-a8af-4eb2-ac63-d82df78283d9',
+            promoId: '112887b0-a8af-4eb2-ac63-d82df78283d9',
+            timing: 40000, // 40 seconds
+            attempts: 40,
+        },
+        3: {
+            name: 'Tile Trio',
+            appToken: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
+            promoId: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
+            timing: 30000, // 20 seconds
+            attempts: 30,
+        },
+        4: {
+            name: 'Mow and Trim',
+            appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+            promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+            timing: 30000, // 20 seconds
+            attempts: 30,
         },
         5: {
             name: 'Chain Cube 2048',
             appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
             promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
-            eventsDelay: 20000,
-            attemptsNumber: 10,
-        },
-        4: {
-            name: 'Cafe Dash',
-            appToken: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
-            promoId: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
-            eventsDelay: 20000,
-            attemptsNumber: 20,
+            timing: 40000, // 30 seconds
+            attempts: 40,
         },
         6: {
             name: 'Train Miner',
             appToken: '82647f43-3f87-402d-88dd-09a90025313f',
             promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
-            eventsDelay: 20000,
-            attemptsNumber: 10,
+            timing: 40000, // 30 seconds
+            attempts: 40,
         },
-        3: {
-            name: 'MergeAway',
+        7: {
+            name: 'Merge Away',
             appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
             promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
-            eventsDelay: 20000,
-            attemptsNumber: 10,
+            timing: 40000, // 30 seconds
+            attempts: 40,
         },
-        2: {
+        8: {
             name: 'Twerk Race 3D',
             appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
             promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
-            eventsDelay: 20000,
-            attemptsNumber: 10,
+            timing: 40000, // 30 seconds
+            attempts: 40,
         },
-        7: {
+        9: {
             name: 'Polysphere',
             appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
             promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
-            eventsDelay: 20000,
-            attemptsNumber: 16,
+            timing: 30000, // 20 seconds
+            attempts: 30,
         },
-        8: {
-            name: 'Mow and Trim',
-            appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
-            promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
-            eventsDelay: 20000,
-            attemptsNumber: 20,
-        },
-        9: {
-            name: 'Mud Racing',
-            appToken: '8814a785-97fb-4177-9193-ca4180ff9da8',
-            promoId: '8814a785-97fb-4177-9193-ca4180ff9da8',
-            eventsDelay: 20000,
-            attemptsNumber: 20,
-        },
+        10: {
+            name: 'Stone Age',
+            appToken: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
+            promoId: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
+            timing: 40000, // 40 seconds
+            attempts: 40,
+        }
     };
 
     // Initialize UI elements
